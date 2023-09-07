cbe7eed849351378db79084d2a15064af30d0500
SAPHURAH <125644293+SAPHURAH@users.noreply.github.com>
2023-09-07 10:46:17 +0000

    SOME TRIALS WITH SQL FOR THE FIRST TIME

---
+++ SQLQuery6.sql
@@ -0,0 +1,18 @@
+INSERT INTO SafiniNice VALUES 
+(1002, 'Female', 'Ama','Ata', 20),
+(1003, 'Male', 'Richard','Ato', 20),
+(1004, 'Female', 'Pruddy','Adams', 20),
+(1005, 'Male', 'Freeman','Droh', 20),
+(1006, 'Female', 'Deborah','Lukymin', 20),
+(1007, 'Female', 'Hairat','Futa', 20),
+(1008, 'Female', 'Safura','Tiyumba', 20)
+
+
+INSERT INTO RubbyLook VALUES
+(2009,'Banku'),
+(2003,'Waakye'),
+(2002,'Tuozaafi'),
+(2001,'Kokontey'),
+(2006,'Ampesi'),
+(2008,'Jollof'),
+(2004,'Rice')
